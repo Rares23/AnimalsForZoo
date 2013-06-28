21a938f64008ef86285566cf58b75c66f1674f8a
rares123 <cruceat.rares@yahoo.com>
2013-06-28 12:29:33 +0300

    Rollback app

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ AnimalsFromZoo::Application.routes.draw do
 
   get "pages/contact", as: "contact"
 
-  get "pages/login", as: "login"
+
   resources :species
 
 
@@ -64,7 +64,7 @@ AnimalsFromZoo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#login'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
